860c63becb5d310b630c5f80dc93abbaab6b3736
Anugrah Pal <anugrah.x.pal.-nd@disney.com>
2018-06-29 08:21:32 -0400

    commit for project

---
+++ src/test/java/com/anu/HelloTest.java
@@ -0,0 +1,15 @@
+package com.anu;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class HelloTest {
+
+	@Test
+	public void testHello(){
+		Assert.assertEquals("HELLO", "HELLO");
+		
+	}
+	
+}
